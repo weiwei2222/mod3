ef7aff61a588c4088573236fc621bb54a775b68d
Weiwei <linweiwei2222@163.com>
2023-12-06 14:15:50 -0500

    practice

--- express-plants/server.js
+++ express-plants/server.js
@@ -9,8 +9,35 @@ const plants = [
   "Witches' Butter",
 ];
 
-app.get("/:indexOfPlantsArrary", (req, res) => {
-  res.send(plants[req.params.indexOfPlantsArrary]);
+const hello = { firstname: "bob", lastname: "bobbybob" };
+
+app.get("/awesome", (req, res) => {
+  //this will never be reached
+  res.send(`
+      <h1>Plants are awesome!</h1>
+      <img src="https://static.boredpanda.com/blog/wp-content/uuuploads/plant-sculptures-mosaicultures-internationales-de-montreal/plant-sculptures-mosaicultures-internationales-de-montreal-14.jpg" >
+    `);
+});
+
+app.get("/hello/:firstname/:lastname", (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send("hello " + req.params.firstname + " " + req.params.lastname);
+});
+
+app.get("/howdy/:firstName", function (req, res) {
+  console.log(req.params);
+  console.log(req.query);
+  res.send("hello " + req.query.title + " " + req.params.firstName);
+});
+app.get("/:indexOfPlantsArray", (req, res) => {
+  if (plants[req.params.indexOfPlantsArray]) {
+    res.send(plants[req.params.indexOfPlantsArray]);
+  } else {
+    res.send(
+      "cannot find anything at this index: " + req.params.indexOfPlantsArray
+    );
+  }
 });
 
 app.listen(port, () => {
