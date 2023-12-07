bd34e69b816f480a007c2cfdf9d5ccf1d428e1d0
Weiwei <linweiwei2222@163.com>
2023-12-07 14:26:27 -0500

    practice

--- fruits_backend_example/server.js
+++ fruits_backend_example/server.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const fruits = require("./models/fruits.js");
 const jsxViewEngine = require("jsx-view-engine");
+
+app.set("view engine", "jsx");
+app.set("views", "./views");
+app.engine("jsx", jsxViewEngine());
+
+// moving the fruits to models/fruits.js in order to have compartmentalized code
+// we are using MVC - models-views-controllers - architecture
+// https://developer.mozilla.org/en-US/docs/Glossary/MVC
+// const fruits = [
+//     {
+//         name:'apple',
+//         color: 'red',
+//         readyToEat: true
+//     },
+//     {
+//         name:'pear',
+//         color: 'green',
+//         readyToEat: false
+//     },
+//     {
+//         name:'banana',
+//         color: 'yellow',
+//         readyToEat: true
+//     }
+// ];
+
+app.use((req, res, next) => {
+  console.log("Middleware: I run for all routes");
+  next();
+});
+//near the top, around other app.use() calls
+app.use(express.urlencoded({ extended: false }));
 
 // These are my routes
 // We are going to create the 7 RESTful routes
@@ -18,19 +49,41 @@ const jsxViewEngine = require("jsx-view-engine");
 // S - Show     GET         READ - display a specific element
 
 app.get("/", (req, res) => {
-  res.send("this is my fruit root route");
+  res.send("this is my fruits root route");
 });
 
 // I - INDEX - dsiplays a list of all fruits
-app.get("/fruits", (req, res) => {
-  res.send(fruits);
+app.get("/fruits/", (req, res) => {
+  // res.send(fruits);
+  res.render("Index", { fruits: fruits });
 });
 
+app.get("/fruits/new", (req, res) => {
+  res.render("New");
+});
+
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  fruits.push(req.body);
+
+  console.log(fruits);
+  res.redirect("/fruits");
+});
 // S - SHOW - show route displays details of an individual fruit
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-  res.send(fruits[req.params.indexOfFruitsArray]);
+  // res.send(fruits[req.params.indexOfFruitsArray]);
+  res.render("Show", {
+    // second parameter must be an object
+    fruit: fruits[req.params.indexOfFruitsArray],
+  });
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log("listening");
 });
--- fruits_backend_example/views/Show.jsx
+++ fruits_backend_example/views/Show.jsx
@@ -1,8 +1,16 @@
-const React = require('react')
-const Show extends React.Component {
-    render(){
+const React = require("react");
+class Show extends React.Component {
+  render() {
+    const fruit = this.props.fruit;
+    return (
+      <div>
         <h1>Show page</h1>
-    }
+        <div>
+          The {fruit.name} is {fruit.color}
+        </div>
+        <div>{fruit.readyToEat ? "It is ready to eat" : "Not ready"}</div>
+      </div>
+    );
+  }
 }
-
 module.exports = Show;
