68ef4bb2b1376518ca99a0d24faec152143a9067
Weiwei <linweiwei2222@163.com>
2024-01-03 19:34:58 -0500

    practice

--- mern-infrastructure/src/pages/App.jsx
+++ mern-infrastructure/src/pages/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import NewOrderPage from "./NewOrderPage";
 import OrderHistoryPage from "./OrderHistoryPage";
+import NavBar from "../components/NavBar";
 
 function App() {
   const [user, setUser] = useState({});
@@ -14,10 +15,19 @@ function App() {
   // the setter function should be named according to converntion
 
   return (
-    <>
-      {user ? <NewOrderPage /> : <AuthPage />}
-      <OrderHistoryPage />
-    </>
+    <main className="App">
+      {user ? (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage />
+      )}
+    </main>
   );
 }
 
